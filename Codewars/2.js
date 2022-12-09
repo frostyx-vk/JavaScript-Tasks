045ec2c040578a66b21e15f080f9962ed29a46a8
frostyx-vk <vk.xam@yandex.ru>
2022-12-09 20:49:14 +0600

    Basic data types--Number

--- Codewars/2.js
+++ Codewars/2.js
@@ -1,6 +1,33 @@
-// https://www.codewars.com/kata/571ec274b1c8d4a61c0000c8
+// https://www.codewars.com/kata/571edd157e8954bab500032d/
 
-function helloWorld() {
-    let str = 'Hello World!';
-    console.log(str)
-  }
+let v1 = 50,
+    v2 = 100,
+    v3 = 150,
+    v4 = 200,
+    v5 = 2,
+    v6 = 250;
+
+function equal1(){
+  let a = v1,   
+      b = v1;   
+  return a + b;
+}
+
+//Please refer to the example above to complete the following functions
+function equal2(){
+  let a = v4 , //set number value to a
+      b = v2 ; //set number value to b
+  return a - b;
+}
+
+function equal3(){
+  let a =v1  , //set number value to a
+      b = v5 ; //set number value to b
+  return a * b;
+}
+
+function equal4(){
+  let a = v4  , //set number value to a
+      b = v5 ; //set number value to b
+  return a / b;
+}
