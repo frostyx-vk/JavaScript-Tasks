e1262a1bf0b248abe72e17a74150de462e55b9d1
frostyx-vk <vk.xam@yandex.ru>
2022-12-09 21:12:14 +0600

    Basic data types--Array

---
+++ Codewars/4.js
@@ -0,0 +1,24 @@
+// https://www.codewars.com/kata/571effabb625ed9b0600107a/
+
+function getLength(arr){
+  let array = arr.length;
+  return array;
+}
+function getFirst(arr){
+  let array = arr[0]
+  return array;
+}
+function getLast(arr){
+  let array = arr[arr.length-1]
+  return array;
+}
+function pushElement(arr){
+  var el=1;
+  arr.push(el);
+  return arr;
+}
+function popElement(arr){
+  arr.pop();
+  return arr;
+}
+
