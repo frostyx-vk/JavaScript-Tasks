5300ebc158eb04affe98a56b2f7911297051d1a8
frostyx-vk <vk.xam@yandex.ru>
2022-12-16 17:28:51 +0600

    Find Maximum and Minimum Values of a List

---
+++ Codewars/24.js
@@ -0,0 +1,11 @@
+// https://www.codewars.com/kata/577a98a6ae28071780000989/
+
+let min = function(list) {
+  list.sort((a, b) => a-b);
+   return list[0];
+}
+
+let max = function(list){
+  list.sort((a, b) => b-a);
+   return list[0];
+}
