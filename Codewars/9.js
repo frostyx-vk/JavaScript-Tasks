d538ad9ccf1fe6107c5acf1b17b7b2da8c565705
frostyx-vk <vk.xam@yandex.ru>
2022-12-17 22:53:36 +0600

    int

--- Codewars/9.js
+++ Codewars/9.js
@@ -1,18 +1,18 @@
 // https://www.codewars.com/kata/57216d4bcdd71175d6000560
 
-function padIt(str, n){
+function padIt(str, n) {
   let stroka = str;
   let z = '*';
-  
-   while(n) {
+
+  while (n) {
     if (n % 2 === 0) {
       stroka = stroka + z;
     } else {
       stroka = z + stroka;
     };
-    
+
     n--;
   };
-  
+
   return stroka;
 };
