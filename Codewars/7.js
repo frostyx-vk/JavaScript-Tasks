22c6686691c5f8de3c57fbe99c95b67116c70669
frostyx-vk <vk.xam@yandex.ru>
2022-12-18 22:37:17 +0600

    Int

--- Codewars/7.js
+++ Codewars/7.js
@@ -1,7 +1,7 @@
 // https://www.codewars.com/kata/57202aefe8d6c514300001fd
 
-function saleHotdogs(n){
-  if(n<5){return 100*n}
-  else if (n>=5&&n<10){return 95*n}
-  else if (n>=10){return 90*n}
+function saleHotdogs(n) {
+  if (n < 5) { return 100 * n }
+  else if (n >= 5 && n < 10) { return 95 * n }
+  else if (n >= 10) { return 90 * n }
 }
