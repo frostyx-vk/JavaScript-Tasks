9941c03ad334aa8a49180da9b80cdebebaba480b
frostyx-vk <vk.xam@yandex.ru>
2022-12-25 19:10:17 +0600

    Int

--- Codewars/4.js
+++ Codewars/4.js
@@ -1,23 +1,23 @@
 // https://www.codewars.com/kata/571effabb625ed9b0600107a/
 
-function getLength(arr){
+function getLength(arr) {
   let array = arr.length;
   return array;
 }
-function getFirst(arr){
+function getFirst(arr) {
   let array = arr[0]
   return array;
 }
-function getLast(arr){
-  let array = arr[arr.length-1]
+function getLast(arr) {
+  let array = arr[arr.length - 1]
   return array;
 }
-function pushElement(arr){
-  var el=1;
+function pushElement(arr) {
+  var el = 1;
   arr.push(el);
   return arr;
 }
-function popElement(arr){
+function popElement(arr) {
   arr.pop();
   return arr;
 }
