dbf055f389e42fd1d6d84897300a2ca39ae63563
frostyx-vk <vk.xam@yandex.ru>
2023-01-15 23:07:32 +0600

    Int

--- Codewars/22.js
+++ Codewars/22.js
@@ -1,16 +1,16 @@
 // https://www.codewars.com/kata/572ab0cfa3af384df7000ff8
 
-function shuffleIt(arr, ...indexesToSwapPairs){
+function shuffleIt(arr, ...indexesToSwapPairs) {
   const array = [...arr];
-  
-  for(const indexesPair of indexesToSwapPairs) {
+
+  for (const indexesPair of indexesToSwapPairs) {
     const [firstIdx, secondIdx] = indexesPair;
     const firstValue = array[firstIdx];
     const secondValue = array[secondIdx];
-    
+
     array[firstIdx] = secondValue;
     array[secondIdx] = firstValue;
-    
+
   }
 
 }
