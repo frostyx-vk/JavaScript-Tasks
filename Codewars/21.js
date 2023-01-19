fa0caf8932301f073186aeeac742bc729dd7001a
frostyx-vk <vk.xam@yandex.ru>
2023-01-19 20:49:34 +0600

    Int

--- Codewars/21.js
+++ Codewars/21.js
@@ -1,11 +1,11 @@
 // https://www.codewars.com/kata/5729b103dd8bac11a900119e
 
-function fiveLine(s){
+function fiveLine(s) {
   let a = s.trim();
-  let b = a+a;
-  let c = a+a+a;
-  let d = a+a+a+a;
-  let e = a+a+a+a+a;
+  let b = a + a;
+  let c = a + a + a;
+  let d = a + a + a + a;
+  let e = a + a + a + a + a;
   let letter = (`${a}\n${b}\n${c}\n${d}\n${e}`)
   return letter;
 }
