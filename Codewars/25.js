6424bd5a2ee4b2090104d031323e294a7bf9a3c1
frostyx-vk <vk.xam@yandex.ru>
2023-01-20 19:58:21 +0600

    Price of Mangoes

---
+++ Codewars/25.js
@@ -0,0 +1,7 @@
+// https://www.codewars.com/kata/57a77726bb9944d000000b06/
+
+function mango(quantity, price){
+  const freeCount = Math.floor(quantity / 3);
+  const payCount = quantity - freeCount
+  return payCount * price
+}
