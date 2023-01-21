538e550cf27560e06fb8f564f03901f7180e33b2
frostyx-vk <vk.xam@yandex.ru>
2023-01-21 20:56:30 +0600

    Int

--- Codewars/25.js
+++ Codewars/25.js
@@ -1,6 +1,6 @@
 // https://www.codewars.com/kata/57a77726bb9944d000000b06/
 
-function mango(quantity, price){
+function mango(quantity, price) {
   const freeCount = Math.floor(quantity / 3);
   const payCount = quantity - freeCount
   return payCount * price
