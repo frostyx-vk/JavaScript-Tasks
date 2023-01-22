64733db65f8aed35bba55e50d233f1bd13f41248
frostyx-vk <vk.xam@yandex.ru>
2023-01-22 22:20:48 +0600

    Even or Odd

---
+++ Codewars/28.js
@@ -0,0 +1,9 @@
+// https://www.codewars.com/kata/53da3dbb4a5168369a0000fe/
+
+function evenOrOdd(number) {
+  if(number%2===0){
+    return number = "Even";
+  } else if(number%2===1 || number%2===-1){
+    return number = "Odd";
+  }
+}
