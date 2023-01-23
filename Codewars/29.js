8fb6a9e6c577d878e2d10c0a0909d079ed03095a
frostyx-vk <vk.xam@yandex.ru>
2023-01-23 12:19:21 +0600

    Sum of odd numbers

---
+++ Codewars/29.js
@@ -0,0 +1,13 @@
+// https://www.codewars.com/kata/55fd2d567d94ac3bc9000064/
+
+function rowSumOddNumbers(n) {
+
+  const start = n ** 2 - n + 1;
+  
+  let sum = start;
+  
+  for (let index = 1; index < n; index++) {
+    sum += start + (2 * index);
+  }
+      return sum
+  }
