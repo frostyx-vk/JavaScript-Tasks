c3a4d03df3a8bf8439875f9caa591a31c27db9b7
frostyx-vk <vk.xam@yandex.ru>
2023-01-24 10:31:12 +0600

    Powers of 2

---
+++ Codewars/37.js
@@ -0,0 +1,11 @@
+// https://www.codewars.com/kata/57a083a57cb1f31db7000028/
+
+function powersOfTwo(n){
+  let a = [];
+  for (let i = 0; i <= n; i++){
+    let b = 2**i;
+    let c = a.push(b);
+  }
+  return a
+}
+
