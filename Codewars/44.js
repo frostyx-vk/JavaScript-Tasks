f16292e2462da45d3d0084a2204557187cfd825a
frostyx-vk <vk.xam@yandex.ru>
2023-01-29 18:12:59 +0600

    Int

--- Codewars/44.js
+++ Codewars/44.js
@@ -1,10 +1,10 @@
 // https://www.codewars.com/kata/57a1fd2ce298a731b20006a4/
 
 function isPalindrome(x) {
-  if(x.slice(0,1).toUpperCase() === x.slice(-1).toUpperCase()){
+  if (x.slice(0, 1).toUpperCase() === x.slice(-1).toUpperCase()) {
     return true
-  } else if(x.slice(0,1).toUpperCase() !== x.slice(-1).toUpperCase()){
-console.log(x)
+  } else if (x.slice(0, 1).toUpperCase() !== x.slice(-1).toUpperCase()) {
+    console.log(x)
     return false
-  } 
+  }
 }
