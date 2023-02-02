0fe49d2bf160e3bdc63ede2b0904973509aaf8ea
frostyx-vk <vk.xam@yandex.ru>
2023-02-02 17:14:35 +0600

    multiplication table

---
+++ Codewars/50.js
@@ -0,0 +1,18 @@
+// таблица умножения в столбик
+
+// Создаем
+let arr = Array(10);
+for (let i = 1; i < 10; i++) {
+  arr[i] = [...Array(10)].map((_, j) => i * j);
+}
+
+console.log(arr)
+
+// Делаем таблицу текстом
+let s = '';
+for (let i = 1; i < 10; i++) {
+  s += arr[i].slice(1).join(' ') + '\n';
+}
+
+console.log(s);         // Вся таблица
+console.log(arr[5][5]); // 25
