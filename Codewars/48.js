2c17ada621a411a6641d23b6f1f9b8e0dd41280b
frostyx-vk <vk.xam@yandex.ru>
2023-02-03 19:40:51 +0600

    Int

--- Codewars/48.js
+++ Codewars/48.js
@@ -1,5 +1,5 @@
 // https://www.codewars.com/kata/563e320cee5dddcf77000158/
 
-function getAverage(marks){
-  return Math.floor(marks.reduce((a, b) => a + b)/marks.length)
+function getAverage(marks) {
+  return Math.floor(marks.reduce((a, b) => a + b) / marks.length)
 }
