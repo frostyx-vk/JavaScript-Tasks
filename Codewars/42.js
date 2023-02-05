8ea40bf9b439146fb7cae4c472d17d3d48ba80f3
frostyx-vk <vk.xam@yandex.ru>
2023-02-05 17:08:33 +0600

    Int

--- Codewars/42.js
+++ Codewars/42.js
@@ -1,7 +1,7 @@
 // https://www.codewars.com/kata/551b4501ac0447318f0009cd/
 
-function booleanToString(b){
-  if(b===true){
+function booleanToString(b) {
+  if (b === true) {
     return 'true'
   } else {
     return 'false'
