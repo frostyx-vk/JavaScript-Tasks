0672793318d3eae2a7c1f69dac380aa517e902c5
frostyx-vk <vk.xam@yandex.ru>
2023-02-11 10:12:53 +0600

    Int

--- Codewars/37.js
+++ Codewars/37.js
@@ -1,9 +1,9 @@
 // https://www.codewars.com/kata/57a083a57cb1f31db7000028/
 
-function powersOfTwo(n){
+function powersOfTwo(n) {
   let a = [];
-  for (let i = 0; i <= n; i++){
-    let b = 2**i;
+  for (let i = 0; i <= n; i++) {
+    let b = 2 ** i;
     let c = a.push(b);
   }
   return a
