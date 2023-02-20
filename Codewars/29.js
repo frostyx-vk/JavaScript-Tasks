2b01a97090c13803b392247218f1f6b16e848442
frostyx-vk <vk.xam@yandex.ru>
2023-02-20 10:16:43 +0600

    Int

--- Codewars/29.js
+++ Codewars/29.js
@@ -3,11 +3,11 @@
 function rowSumOddNumbers(n) {
 
   const start = n ** 2 - n + 1;
-  
+
   let sum = start;
-  
+
   for (let index = 1; index < n; index++) {
     sum += start + (2 * index);
   }
-      return sum
-  }
+  return sum
+}
