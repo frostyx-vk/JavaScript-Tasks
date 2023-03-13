052c7b66b1741fcadd51874025c1af0e134aade8
frostyx-vk <vk.xam@yandex.ru>
2023-03-13 08:53:02 +0600

    Int

--- Codewars/53.js
+++ Codewars/53.js
@@ -1,12 +1,13 @@
 // https://www.codewars.com/kata/562926c855ca9fdc4800005b/
 
-function numberToPower(number, power){
+function numberToPower(number, power) {
   let result = number;
-  if(power === 0){
+  if (power === 0) {
     result = 1
   } else {
-  for (let i = 1; i < power; i++) {
-    result *= number;
-  }}
+    for (let i = 1; i < power; i++) {
+      result *= number;
+    }
+  }
   return result;
 }
