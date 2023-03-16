98bcf09527d67f3e9f38ecfa0b5602c88ca417fe
frostyx-vk <vk.xam@yandex.ru>
2023-03-16 09:23:12 +0600

    Int

--- Codewars/55.js
+++ Codewars/55.js
@@ -1,4 +1,4 @@
-// https://www.codewars.com/kata/5703c093022cd1aae90012c9/
+// https://www.codewars.com/kata/5703c093022cd1aae90012c9
 
 function find(arr, e) {
   return arr.includes(e) ? arr.indexOf(e) : "Not found"
