40665430c3eeec8f5096fd652d5a52d712bdb9a2
frostyx-vk <vk.xam@yandex.ru>
2023-03-21 08:23:55 +0700

    Int

--- Codewars/53.js
+++ Codewars/53.js
@@ -1,4 +1,4 @@
-// https://www.codewars.com/kata/562926c855ca9fdc4800005b/
+// https://www.codewars.com/kata/562926c855ca9fdc4800005b
 
 function numberToPower(number, power) {
   let result = number;
