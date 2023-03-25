5afe407c52e91d135ac89c38d9c72048b15ef519
frostyx-vk <vk.xam@yandex.ru>
2023-03-25 16:13:34 +0700

    Int

--- Codewars/47.js
+++ Codewars/47.js
@@ -1,4 +1,4 @@
-// https://www.codewars.com/kata/56bc28ad5bdaeb48760009b0/
+// https://www.codewars.com/kata/56bc28ad5bdaeb48760009b0
 
 function removeChar(str) {
   return a = str.slice(1, str.length - 1);
