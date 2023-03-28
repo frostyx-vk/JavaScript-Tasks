b6e0d3cd9b7926ad12855ff2fc98c1e7639a75ba
frostyx-vk <vk.xam@yandex.ru>
2023-03-28 08:41:28 +0700

    Int

--- Codewars/44.js
+++ Codewars/44.js
@@ -1,4 +1,4 @@
-// https://www.codewars.com/kata/57a1fd2ce298a731b20006a4/
+// https://www.codewars.com/kata/57a1fd2ce298a731b20006a4
 
 function isPalindrome(x) {
   if (x.slice(0, 1).toUpperCase() === x.slice(-1).toUpperCase()) {
