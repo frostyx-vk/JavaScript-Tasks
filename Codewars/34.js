0c63f62799222541d4bd72ad847cb19a7b88a120
frostyx-vk <vk.xam@yandex.ru>
2023-04-03 09:13:44 +0700

    Int

--- Codewars/34.js
+++ Codewars/34.js
@@ -1,4 +1,4 @@
-// https://www.codewars.com/kata/5899dc03bc95b1bf1b0000ad/
+// https://www.codewars.com/kata/5899dc03bc95b1bf1b0000ad
 
 function invert(array) {
   let a = array.map(item => item * -1)
