5eb8624c4ff4e54d06771a78dd7a61a237909e36
frostyx-vk <vk.xam@yandex.ru>
2023-04-12 11:54:46 +0700

    Int

--- Codewars/25.js
+++ Codewars/25.js
@@ -1,4 +1,4 @@
-// https://www.codewars.com/kata/57a77726bb9944d000000b06/
+// https://www.codewars.com/kata/57a77726bb9944d000000b06
 
 function mango(quantity, price) {
   const freeCount = Math.floor(quantity / 3);
